92e5337163050141da0726e96b83d9cb0ce1d757
Madeline Ritchie <2144273+MadelineRitchie@users.noreply.github.com>
2024-03-03 02:31:05 -0800

    jubilee init

---
+++ scripts/bootstrap-new-nixos.ps1
@@ -0,0 +1,9 @@
+#!/usr/bin/env pwsh
+$ErrorActionPreference = 'Stop'
+
+# if machine exists then bail
+# create machine folder
+# move nixos config files
+# symbolic link them
+# 
+write-host 'ice breaker'
---
+++ scripts/bootstrap-new-nixos.sh
@@ -0,0 +1,2 @@
+nix-shell -p fish tmux neovim powershell tldr wl-clipboard jujutsu
+echo "ok you're in PowerShell now. Time to run the ps1 version of this script"
